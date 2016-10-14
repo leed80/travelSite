ab96c537e97217a7e5060fb3cf1c717439fdb00f
David <d.c.s.lee-09@alumni.lboro.ac.uk>
2016-10-14 19:47:47 +0100

    removes an outdated function that set the user status to false

--- reg/forms.py
+++ reg/forms.py
@@ -27,12 +27,3 @@ def clean_email(self):
         return email
     raise forms.ValidationError('duplicate email')
 
-#modify save() method so that we can set user.is_active to False when we first create our user
-def save(self, commit=True):        
-    user = super(RegistrationForm, self).save(commit=False)
-    user.email = self.cleaned_data['email']
-    if commit:
-        user.is_active = False # not active until he opens activation link
-        user.save()
-
-    return user
