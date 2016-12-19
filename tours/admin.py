0d6ad771fc1d556c5d26e86409c5536c0c9e1ab9
David <d.c.s.lee-09@alumni.lboro.ac.uk>
2016-12-19 18:19:37 +0000

    obsolete

--- tours/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from models import tourTable, stopTable, activities
-
-admin.site.register(tourTable)
-admin.site.register(stopTable)
-admin.site.register(activities)
