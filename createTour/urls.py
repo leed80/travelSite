2457e05d2f4aa124b80426b6f2111a865ce1e1b8
David <d.c.s.lee-09@alumni.lboro.ac.uk>
2017-02-16 19:16:47 +0000

    added the session.modified = True so that it would save the session_key

--- createTour/urls.py
+++ createTour/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-     url(r'^$', views.details, name='selected details'),
-     url(r'^itinerarycrud', views.itineraryCRUD),
-     url(r'^hotels/', views.hotels, name="hotels"),
-     
+    url(r'^$', views.details, name='selected details'),
+    url(r'^itinerarycrud', views.itineraryCRUD),
+    url(r'^hotels/', views.hotels, name="hotels"),
+
 ]
